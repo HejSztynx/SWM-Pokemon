018702230d3e89a87f6d6e85e61c3e13c5d29754
Krystian Sienkiewicz <krystiansienkiewicz@MacBook-Pro-Krystian.local>
2026-01-09 11:53:38 +0100

    list prototype

---
+++ pokemon-app/app/index.tsx
@@ -0,0 +1,5 @@
+import PokemonList from "@/components/PokemonList";
+
+export default function Index() {
+  return <PokemonList />;
+}
---
+++ pokemon-app/components/PokemonList.tsx
@@ -0,0 +1,117 @@
+import { FlashList } from "@shopify/flash-list";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Image } from "expo-image";
+import { useMemo, useState } from "react";
+import { ActivityIndicator, RefreshControl, Text, View } from "react-native";
+
+interface PokemonData {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+interface Page {
+  pokemons: PokemonData[];
+  nextPage: number;
+}
+
+const urlBase = "https://pokeapi.co/api/v2/pokemon/";
+
+const fetchPokemonData = (id: number): Promise<PokemonData | null> => {
+  return fetch(`${urlBase}${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => ({
+      id: data.id.toString(),
+      name: data.name,
+      imageUrl: data.sprites.front_default,
+    }))
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+export default function TestList() {
+  // const [message, setMessage] = useState<string>("nie wiem");
+
+  const fetchPokemons = async (pageParam: number = 0) => {
+    try {
+      const ids = Array.from({ length: 20 }, (_, i) => pageParam * 20 + i + 1);
+      const pokemonPromises = ids.map((id) => fetchPokemonData(id));
+      const pokemonResults = await Promise.all(pokemonPromises);
+      return pokemonResults.filter((item) => item !== null);
+    } catch (err) {
+      // setMessage(err.message);
+      console.error("Error fetching pokemons", err);
+      return [];
+    }
+  };
+
+  const fetchPage = async ({ pageParam = 0 }): Promise<Page> => {
+    const pokemons: PokemonData[] = await fetchPokemons(pageParam);
+    console.log("page " + pageParam);
+
+    return { pokemons, nextPage: pageParam + 1 };
+  };
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch,
+    isRefetching,
+  } = useInfiniteQuery({
+    queryKey: ["pokemons"],
+    initialPageParam: 0,
+    queryFn: fetchPage,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+
+  const pokemons = useMemo(
+    () => data?.pages.flatMap((page) => page.pokemons) || [],
+    [data]
+  );
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <FlashList
+        data={pokemons}
+        keyExtractor={(item) => item.id}
+        refreshControl={
+          <RefreshControl
+            tintColor={"blue"}
+            refreshing={isRefetching}
+            onRefresh={refetch}
+          />
+        }
+        renderItem={({ item }) => (
+          // <Text>{item.name}</Text>
+          <Image
+            source={{ uri: item.imageUrl }}
+            style={{ width: 200, height: 200, marginVertical: 5 }}
+            cachePolicy="memory-disk"
+          />
+        )}
+        onEndReachedThreshold={0.2}
+        onEndReached={() =>
+          hasNextPage && !isFetchingNextPage && fetchNextPage()
+        }
+        ListFooterComponent={
+          isFetchingNextPage ? (
+            <ActivityIndicator
+              color="blue"
+              size="small"
+              style={{ marginBottom: 5 }}
+            />
+          ) : null
+        }
+      />
+    </View>
+  );
+}
